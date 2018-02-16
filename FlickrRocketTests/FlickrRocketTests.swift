408beee2d35ba955f02fd21bdd4c03a401f53d79
jbaumann <jbaumann@pauls-mac-mini.udallas.edu>
2018-02-16 10:32:26 -0800

    Initial Commit

---
+++ FlickrRocketTests/FlickrRocketTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlickrRocketTests.swift
+//  FlickrRocketTests
+//
+//  Created by jbaumann on 2/16/18.
+//  Copyright © 2018 jbaumann. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrRocket
+
+class FlickrRocketTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
